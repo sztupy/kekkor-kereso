9c72da07f5cbf44327ae83d4d812cd1ed6fc564f
Zsolt Sz. Sztupák <mail@sztupy.hu>
2023-09-12 13:37:05 +0100

    New features: list of results, opacity toggle, map search
    New features:
    
    * List of results: new button that shows the list of visible results. Adds the ability to order them as well
    * Opacity toggle: start-time setting enabling/disabling transparency for slower devices
    * Map search: ability to restrict results to the visible map

--- files/main.js
+++ files/main.js
@@ -6,13 +6,14 @@ import View from 'ol/View.js';
 import {Stroke, Style} from 'ol/style.js';
 import {OSM, Vector as VectorSource} from 'ol/source.js';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
-import {useGeographic, toLonLat} from 'ol/proj.js';
+import {useGeographic} from 'ol/proj.js';
 import Select from 'ol/interaction/Select'
-import {pointerMove} from 'ol/events/condition.js';
+import {click, pointerMove} from 'ol/events/condition.js';
 import Overlay from 'ol/Overlay';
 import {Control, defaults as defaultControls} from 'ol/control.js';
 import { MultiLineString } from 'ol/geom';
 import GPX from 'ol/format/GPX.js';
+import { containsExtent } from 'ol/extent';
 
 const resultsDom = document.getElementById("results");
 const resultsTextDom = document.getElementById("resulttext");
@@ -29,6 +30,9 @@ const closer = document.getElementById('popup-closer');
 
 const form = document.getElementById('search-form')
 
+const resultList = document.getElementById('result-list');
+const resultListContent = document.getElementById('result-list-content');
+
 const layers = [];
 const mipMapLayers = [
   { maxZoom: 9, sources: [] },
@@ -38,48 +42,75 @@ const mipMapLayers = [
 ];
 let currentZoom = 1;
 
-// no mipmap
-// const mipMapLayers = [
-//   { maxZoom: Number.NEGATIVE_INFINITY, sources: [] }
-// ];
-// let currentZoom = 0;
+let defaultOpacity;
+let hoverOpacity;
+let selectedOpacity;
+let defaultFeatureOpacity;
+let selectedFeatureOpacity;
 
 let visibleLayerCount = 0;
+let visibleLayers = [];
 
 let mouseCoord = null;
 let selectedFeature = null;
+let listHoverLayer = null;
 
 useGeographic();
+
+function setOpacity(enabled) {
+  if (enabled) {
+    defaultOpacity = 0.5;
+    hoverOpacity = 1;
+    selectedOpacity = 1;
+    defaultFeatureOpacity = 0.5;
+    selectedFeatureOpacity = 0.75;
+  } else {
+    defaultOpacity = 1;
+    hoverOpacity = 1;
+    selectedOpacity = 1;
+    defaultFeatureOpacity = 1;
+    selectedFeatureOpacity = 1;
+  }
+}
+
+setOpacity(!window.location.search.includes("opaque"));
 
 function dataToString(selectedFeature, data, advanced) {
   let featureString = '';
   const layerData = data.get("data");
 
-  if (selectedFeature.get('type') == 'blue_route') {
-    featureString = `<b>Kékkör</b>: ${Math.round(layerData.blue_length)/1000}km<br><b>Start</b>:${layerData.end_stamp.desc}<br><b>Cél</b>:${layerData.start_stamp.desc}`;
-  } else if (selectedFeature.get('type') == 'walk_route_start') {
-    featureString = `<b>Séta</b>: ${Math.round(layerData.walk_start_length)/1000}km<br><b>Start</b>: ${layerData.start_stamp.desc}<br><b>Cél</b>: ${layerData.start_transit}`;
-  } else if (selectedFeature.get('type') == 'walk_route_end') {
-    featureString = `<b>Séta</b>: ${Math.round(layerData.walk_end_length)/1000}km<br><b>Start</b>:${layerData.end_transit}<br><b>Cél</b>: ${layerData.end_stamp.desc}`;
-  } else if (selectedFeature.get('type') == 'transit') {
-    const featureData = selectedFeature.get('data');
-    featureString = `<b>Tömegközlekedés</b>: ${Math.round(featureData.duration/60)} perc<br><b>Járat</b>: ${featureData.route.routeShortName}<br><b>Start</b>: ${featureData.route.from.name}<br><b>Cél</b>: ${featureData.route.to.name}<br><b>Indulás</b>: ${featureData.start_times.join(", ")}<br><b>Érkezés</b>: ${featureData.end_times.join(", ")}`;
-  } else if (selectedFeature.get('type') == 'transit_walk') {
-    const featureData = selectedFeature.get('data');
-    featureString = `<b>Átszállás</b>: ${Math.round(featureData.route.distance)}m<br><b>Start</b>: ${featureData.route.from.name}<br><b>Cél</b>:${featureData.route.to.name}`;
+  if (selectedFeature) {
+    if (selectedFeature.get('type') == 'blue_route') {
+      featureString = `<b>Kékkör</b>: ${Math.round(layerData.blue_length)/1000}km<br><b>Start</b>:${layerData.end_stamp.desc}<br><b>Cél</b>:${layerData.start_stamp.desc}`;
+    } else if (selectedFeature.get('type') == 'walk_route_start') {
+      featureString = `<b>Séta</b>: ${Math.round(layerData.walk_start_length)/1000}km<br><b>Start</b>: ${layerData.start_stamp.desc}<br><b>Cél</b>: ${layerData.start_transit}`;
+    } else if (selectedFeature.get('type') == 'walk_route_end') {
+      featureString = `<b>Séta</b>: ${Math.round(layerData.walk_end_length)/1000}km<br><b>Start</b>:${layerData.end_transit}<br><b>Cél</b>: ${layerData.end_stamp.desc}`;
+    } else if (selectedFeature.get('type') == 'transit') {
+      const featureData = selectedFeature.get('data');
+      featureString = `<b>Tömegközlekedés</b>: ${Math.round(featureData.duration/60)} perc<br><b>Járat</b>: ${featureData.route.routeShortName}<br><b>Start</b>: ${featureData.route.from.name}<br><b>Cél</b>: ${featureData.route.to.name}<br><b>Indulás</b>: ${featureData.start_times.join(", ")}<br><b>Érkezés</b>: ${featureData.end_times.join(", ")}`;
+    } else if (selectedFeature.get('type') == 'transit_walk') {
+      const featureData = selectedFeature.get('data');
+      featureString = `<b>Átszállás</b>: ${Math.round(featureData.route.distance)}m<br><b>Start</b>: ${featureData.route.from.name}<br><b>Cél</b>:${featureData.route.to.name}`;
+    }
   }
 
-  let layerString = `<b>${layerData.end_stamp.name} - ${layerData.start_stamp.name}</b>`;
-
-  if (!advanced)
-    return `${layerString}<br><br>${featureString}`;
+  if (!advanced && selectedFeature)
+    return `<b>${layerData.end_stamp.name} - ${layerData.start_stamp.name}</b><br><br>${featureString}`;
 
   let countChange = layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? 0 : 1),0) - 1;
+  let totalTransit = Math.round(calculateTotalBus(layerData)/60);
+  let totalWalk = Math.round(calculateTotalWalk(layerData))/1000;
+  let blueWalk = Math.round(layerData.blue_length)/1000;
 
-  let advancedLayerString = `<h1>${layerData.end_stamp.name} - ${layerData.start_stamp.name}</h1><br><b>Kiindulópont</b>: ${layerData.start_transit} @ ${layerData.transit_legs[0].start_times.join(" / ")}<br><b>Tömegközlekedés:</b> ${Math.round(layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? 0 : c.route.duration),0)/60)} perc ${countChange > 0 ? `(+ ${countChange} átszállás)`: ''}<br><b>Gyaloglás</b>: ${Math.round(layerData.blue_length + layerData.walk_start_length + layerData.walk_end_length + layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? c.route.distance : 0),0))/1000}km<br><b>Ebből kékkör</b>: ${Math.round(layerData.blue_length)/1000}km`;
+  if (!advanced) {
+    return `<b>${layerData.end_stamp.name} - ${layerData.start_stamp.name}</b><br>🚶 ${totalWalk}km / 🟦 ${blueWalk}km / 🚌 ${totalTransit} perc${countChange>0 ? ` 🔄 ${countChange}` : ''}<br>⏰ ${layerData.transit_legs[0].start_times.join(" / ")}<br><br>`;
+  }
+
+  let advancedLayerString = `<h1>${layerData.end_stamp.name} - ${layerData.start_stamp.name}</h1><br><b>Kiindulópont</b>: ${layerData.start_transit} @ ${layerData.transit_legs[0].start_times.join(" / ")}<br><b>Tömegközlekedés:</b> ${totalTransit} perc ${countChange > 0 ? `(+ ${countChange} átszállás)`: ''}<br><b>Gyaloglás</b>: ${totalWalk}km<br><b>Ebből kékkör</b>: ${blueWalk}km`;
 
   let allSections = `<h3>Kiindulópont</h3>${layerData.start_transit}<br><br>`;
-  allSections += `<h3>Tömegközlekedés</h3><b>Összesen</b>: ${Math.round(layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? 0 : c.route.duration),0)/60)} perc ${countChange > 0 ? `(+ ${countChange} átszállás)`: ''}<br><br>`
+  allSections += `<h3>Tömegközlekedés</h3><b>Összesen</b>: ${totalTransit} perc ${countChange > 0 ? `(+ ${countChange} átszállás)`: ''}<br><br>`
 
   for (let legId=0; legId<layerData.transit_legs.length; legId++) {
     let leg = layerData.transit_legs[legId];
@@ -101,7 +132,7 @@ function dataToString(selectedFeature, data, advanced) {
 
   allSections += `<h3>Séta a bélyegzőhelyre</h3><b>Hossz</b>: ${Math.round(layerData.walk_end_length)/1000}km<br><b>Start</b>:${layerData.end_transit}<br><b>Cél</b>: ${layerData.end_stamp.desc}<br><br>`;
 
-  allSections += `<h3>Kéktúra szakasz</h3><b>Hossz</b>: ${Math.round(layerData.blue_length)/1000}km<br><b>Start</b>:${layerData.end_stamp.name}<br><b>Cél</b>: ${layerData.start_stamp.name}<br><br>`;
+  allSections += `<h3>Kéktúra szakasz</h3><b>Hossz</b>: ${blueWalk}km<br><b>Start</b>:${layerData.end_stamp.name}<br><b>Cél</b>: ${layerData.start_stamp.name}<br><br>`;
 
   allSections += `<h3>Séta vissza a kiindulópontra</h3><b>Hossz</b>: ${Math.round(layerData.walk_start_length)/1000}km<br><b>Start</b>:${layerData.start_stamp.desc}<br><b>Cél</b>: ${layerData.start_transit}<br><br>`;
 
@@ -113,10 +144,17 @@ function hideLayer() {
 
   const layer = layers[selectedFeature.get('layerId')];
 
+  const position = visibleLayers.indexOf(layer);
+  if (position>=0) {
+    visibleLayers.splice(position,1);
+  }
+
   layer.setVisible(false);
   selectedFeature = null;
 
   resultsDom.classList.remove('contents');
+
+  fillVisibleList();
 
   return false;
 }
@@ -165,11 +203,83 @@ function setButtonFunctions() {
   }
 }
 
+function visibleListHover(hoverLayer) {
+  let selectedLayer = null;
+  if (selectedFeature) {
+    selectedLayer = layers[selectedFeature.get('layerId')];
+  }
+
+  for (let vl of visibleLayers) {
+    if (vl!=selectedLayer) {
+      vl.setOpacity(defaultOpacity);
+      vl.changed();
+    }
+  }
+
+  if (hoverLayer) {
+    hoverLayer.setOpacity(hoverOpacity);
+  }
+
+  listHoverLayer = hoverLayer;
+}
+
+function fillVisibleList() {
+  resultListContent.innerHTML = '';
+
+  for (const layer of visibleLayers) {
+    let a = document.createElement('a');
+    a.href = "#";
+    a.classList.add('result-link');
+    a.onmouseover = () => { visibleListHover(layer); return false; }
+    a.onmouseout = () => { visibleListHover(null); return false; }
+    a.onclick = () => { clickFeature(layer.getSource().getFeatures()[0]); return false; }
+    a.innerHTML = dataToString(null,layer,false);
+
+    if (selectedFeature && layers[selectedFeature.get('layerId')] == layer) {
+      a.classList.add('selected-link');
+    }
+
+    resultListContent.appendChild(a);
+  }
+
+}
+
+function calculateTotalWalk(layerData) {
+  return layerData.blue_length + layerData.walk_start_length + layerData.walk_end_length + layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? c.route.distance : 0),0)
+}
+
+function calculateTotalBus(layerData) {
+  return layerData.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? 0 : c.route.duration),0);
+}
+
+function orderList(type) {
+  if (type == 0) {
+    visibleLayers.sort((a,b) => a.get('data').end_stamp.name.localeCompare(b.get('data').end_stamp.name));
+  }
+  if (type == 1) {
+    visibleLayers.sort((a,b) => calculateTotalWalk(a.get('data')) - calculateTotalWalk(b.get('data')));
+  }
+  if (type == 2) {
+    visibleLayers.sort((a,b) => a.get('data').blue_length - b.get('data').blue_length);
+  }
+  if (type == 3) {
+    visibleLayers.sort((a,b) => calculateTotalBus(a.get('data')) - calculateTotalBus(b.get('data')));
+  }
+  if (type == 4) {
+    visibleLayers.sort((a,b) => a.get('data').transit_legs[0].start_times[0].localeCompare(b.get('data').transit_legs[0].start_times[0]));
+  }
+
+  fillVisibleList();
+
+  return false;
+}
+
 function runFilter() {
   let maxWalk = parseInt(form.elements.namedItem('max-walk').value)*1000;
   const minBlue = parseInt(form.elements.namedItem('min-blue').value)*1000;
   const maxBus = parseInt(form.elements.namedItem('max-bus').value)*60;
   const searchType = form.elements.namedItem('search-type').value;
+  const searchExtent = form.elements.namedItem('search-extent').value;
   const amount = parseInt(form.amount.value);
 
   if (maxWalk <= minBlue) {
@@ -178,18 +288,31 @@ function runFilter() {
   }
 
   let validLayers = [];
+  var visibleExtent = map.getView().calculateExtent();
 
   for (let layer of layers) {
     layer.setVisible(false);
     const data = layer.get('data');
-    const totalWalk = data.blue_length + data.walk_start_length + data.walk_end_length + data.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? c.route.distance : 0),0);
-    const totalBus = data.transit_legs.reduce((a,c) => a + (c.route.mode == 'WALK' ? 0 : c.route.duration),0);
-
-
-//    console.log([data.blue_length >= minBlue, totalWalk <= maxWalk]);
+    const totalWalk = calculateTotalWalk(data);
+    const totalBus = calculateTotalBus(data);
 
     if (data.blue_length >= minBlue && totalWalk <= maxWalk && maxBus >= totalBus && (searchType == 'all' || data.type.includes(searchType))) {
-      validLayers.push(layer);
+      if (searchExtent == 'map-touch') {
+        let features = layer.getSource().getFeatures();
+        for (let feature of features) {
+          if (feature.getGeometry().intersectsExtent(visibleExtent)) {
+            validLayers.push(layer);
+            break;
+          }
+        }
+      } else if (searchExtent == 'map-extent') {
+        let features = layer.getSource().getExtent();
+        if (containsExtent(visibleExtent,features)) {
+          validLayers.push(layer);
+        }
+      } else {
+        validLayers.push(layer);
+      }
     }
   }
 
@@ -199,10 +322,18 @@ function runFilter() {
   }
 
   visibleLayerCount = 0;
+  visibleLayers = [];
   for (let i=0; i< Math.min(amount, validLayers.length); i++) {
     validLayers[i].setVisible(true);
+    validLayers[i].changed();
+    for (let features of validLayers[i].getSource().getFeatures()) {
+      features.changed();
+    }
     visibleLayerCount += 1;
+    visibleLayers.push(validLayers[i]);
   }
+
+  fillVisibleList();
 }
 
 const selectStyle = [
@@ -223,15 +354,15 @@ const selectStyle = [
 ];
 
 const styleFunction = function (feature) {
-  if (selectedFeature) {
+  if (selectedFeature && feature.get('id') == selectedFeature.get('id')) {
+    return selectStyle;
+  }
+
+  if (selectedFeature || listHoverLayer) {
     const layer = layers[feature.get('layerId')];
-    const selectedLayer = layers[selectedFeature.get('layerId')];
+    const selectedLayer = selectedFeature ? layers[selectedFeature.get('layerId')] : null;
 
-    if (feature.get('id') == selectedFeature.get('id')) {
-      return selectStyle;
-    }
-
-    if (layer == selectedLayer) {
+    if (layer == selectedLayer || layer == listHoverLayer) {
       return [
         new Style({
           stroke: new Stroke({
@@ -242,9 +373,9 @@ const styleFunction = function (feature) {
         }),
         new Style({
           stroke: new Stroke({
-            color: feature.get('type') == 'blue_route' ? [0,0,255,0.75] :
-                   feature.get('type') == 'transit' ? [255,165,0,0.75] :
-                   [1,50,32,0.75],
+            color: feature.get('type') == 'blue_route' ? [0,0,255,selectedFeatureOpacity] :
+                   feature.get('type') == 'transit' ? [255,165,0,selectedFeatureOpacity] :
+                   [1,50,32,selectedFeatureOpacity],
             width: 4,
           }),
           zIndex: 1001
@@ -254,9 +385,9 @@ const styleFunction = function (feature) {
   }
   return new Style({
     stroke: new Stroke({
-      color: feature.get('type') == 'blue_route' ? [0,0,255,0.5] :
-             feature.get('type') == 'transit' ? [255,165,0,0.5] :
-             [1,50,32,0.5],
+      color: feature.get('type') == 'blue_route' ? [0,0,255,defaultFeatureOpacity] :
+             feature.get('type') == 'transit' ? [255,165,0,defaultFeatureOpacity] :
+             [1,50,32,defaultFeatureOpacity],
       width: 6,
     })
   });
@@ -313,6 +444,36 @@ class ShowHelpControl extends Control {
   showHelpPage() {
     helpPage.style.display = 'block';
     searchPage.style.display = 'none';
+    resultList.style.display = 'none';
+  }
+}
+
+class ShowResultsControl extends Control {
+  constructor(opt_options) {
+    const options = opt_options || {};
+
+    const button = document.createElement('button');
+    button.innerHTML = '🗒️';
+
+    const element = document.createElement('div');
+    element.className = 'show-list ol-unselectable ol-control';
+    element.appendChild(button);
+
+    super({
+      element: element,
+      target: options.target,
+    });
+
+    button.addEventListener('click', this.showResultsControl.bind(this), false);
+  }
+
+  showResultsControl() {
+    if (resultList.style.display == 'block') {
+      resultList.style.display = 'none';
+    } else {
+      helpPage.style.display = 'none';
+      resultList.style.display = 'block';
+    }
   }
 }
 
@@ -324,12 +485,12 @@ const selectPointerMove = new Select({
 selectPointerMove.on("select", event => {
   for (let feature of event.deselected) {
     if (!selectedFeature || selectedFeature.get('layerId') != feature.get('layerId')) {
-      layers[feature.get('layerId')].setOpacity(0.5);
+      layers[feature.get('layerId')].setOpacity(defaultOpacity);
       layers[feature.get('layerId')].setZIndex(1);
     }
   }
   for (let feature of event.selected) {
-    layers[feature.get('layerId')].setOpacity(1);
+    layers[feature.get('layerId')].setOpacity(hoverOpacity);
     layers[feature.get('layerId')].setZIndex(1000);
   }
 
@@ -352,8 +513,12 @@ closer.onclick = function () {
   return false;
 };
 
+for (let orderType = 0; orderType <= 4; orderType++) {
+  document.getElementById(`result-order-${orderType}`).onclick = () => orderList(orderType);
+}
+
 const map = new Map({
-  controls: defaultControls().extend([new ShowSearchControl(), new ShowHelpControl()]),
+  controls: defaultControls().extend([new ShowSearchControl(), new ShowHelpControl(), new ShowResultsControl()]),
   layers: [
     new TileLayer({
       source: new OSM(),
@@ -369,14 +534,7 @@ const map = new Map({
 map.addInteraction(selectPointerMove);
 map.addOverlay(overlay);
 
-map.on('click', function (e) {
-
-  // support for touch devices
-  const clickedFeature = map.getFeaturesAtPixel(e.pixel)[0];
-
-  helpPage.style.display = 'none';
-  searchPage.style.display = 'none';
-
+function clickFeature(clickedFeature) {
   if (clickedFeature) {
     const layer = layers[clickedFeature.get('layerId')];
     resultsTextDom.innerHTML = dataToString(clickedFeature, layer, true);
@@ -384,24 +542,36 @@ map.on('click', function (e) {
     if (!selectedFeature || selectedFeature != clickedFeature) {
       map.getView().fit(layer.getSource().getExtent(), { duration: 1000, padding: [25,25,25,25] });
     } else {
-      layers[selectedFeature.get('layerId')].setOpacity(0.5);
+      layers[selectedFeature.get('layerId')].setOpacity(defaultOpacity);
       layers[selectedFeature.get('layerId')].changed();
     }
 
     selectedFeature = clickedFeature;
-    layers[selectedFeature.get('layerId')].setOpacity(1);
+    layers[selectedFeature.get('layerId')].setOpacity(selectedOpacity);
     layers[selectedFeature.get('layerId')].changed();
   } else {
     resultsDom.classList.remove('contents');
     if (selectedFeature) {
       let oldFeature = selectedFeature;
       selectedFeature = null;
-      layers[oldFeature.get('layerId')].setOpacity(0.5);
+      layers[oldFeature.get('layerId')].setOpacity(defaultOpacity);
       layers[oldFeature.get('layerId')].changed();
     }
   }
 
   setButtonFunctions();
+  fillVisibleList();
+}
+
+map.on('click', function (e) {
+  // support for touch devices
+  const clickedFeature = map.getFeaturesAtPixel(e.pixel)[0];
+
+  clickFeature(clickedFeature);
+
+  helpPage.style.display = 'none';
+  searchPage.style.display = 'none';
+  resultList.style.display = 'none';
 });
 
 map.on('pointermove', function(evt){
@@ -480,7 +650,7 @@ for (let optionId in Data) {
 
   vectorLayer.setSource(mipMapLayers[currentZoom].sources[optionId]);
   vectorLayer.setVisible(false);
-  vectorLayer.setOpacity(0.5);
+  vectorLayer.setOpacity(defaultOpacity);
   vectorLayer.setZIndex(1);
   map.addLayer(vectorLayer);
 
